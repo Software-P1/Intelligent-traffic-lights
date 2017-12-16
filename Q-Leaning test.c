09be6a5b91d5cb933e1f4fc21775c6236f288f00
karlog100 <benno1benno@gmail.com>
2017-12-16 13:07:22 +0100

    Messing with stuff unsabel version
    Trying to get Q-Learning to work this is not even close to be a working version.

---
+++ Q-Leaning test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define nStates 6
+Q[nStates];
+
+/*Choose action*/
+int chooseAction(Q, s) {
+  return 0;
+}
+
+
+
+/* The main function */
+int main(void) {
+
+  return 0;
+}
